ee9e632ce11941278581fc54915d96c900f46b8e
Omar Zouine <zouineomar@localhost>
2024-05-09 19:15:01 -0400

    2ere commit

---
+++ src/bonjournoms/BonjourNoms.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournoms;
+
+/**
+ *
+ * @author zouineomar
+ */
+import aleatoire.Person;
+public class BonjourNoms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hellooo world !");
+    }
+    
+}
